80bab1854d7168d3331f279803475f7b115a7f8f
gsllchb <gsllchb@icloud.com>
2017-11-15 19:07:13 +0800

    修复BUG，添加dill到依赖项；添加.gitignore.

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 import pylf
 setup(
     name='pylf',
-    version='0.1.0',
+    version='0.1.1',
     author='Gsllchb',
     author_email="Gsllchb@gmail.com",
     packages=find_packages(),
-    install_requires=['pillow>=4.3.0', 'multiprocess>=0.70.5'],
+    install_requires=['pillow>=4.3.0', 'multiprocess>=0.70.5', 'dill>=0.2.7'],
     python_requires='>=3.5',
     description="Lightweight and fast Python library for simulating Chinese handwriting.",
     long_description=pylf.__doc__,
