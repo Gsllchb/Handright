349ee777a63233973bab17092efd8f9270612096
gsllchb <gsllchb@icloud.com>
2017-11-16 11:49:06 +0800

    add classifier in setup.py;etc.

--- setup.py
+++ setup.py
@@ -1,5 +1,16 @@
 from setuptools import setup, find_packages
 import pylf
+classifiers=[
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    'Topic :: Software Development :: Build Tools',
+    "Topic :: Multimedia :: Graphics",
+    'License :: OSI Approved :: BSD License',
+    "Topic :: Multimedia :: Graphics",
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    "Programming Language :: Python :: 3.6",
+]
 setup(
     name='pylf',
     version='0.1.1',
@@ -8,7 +19,7 @@ setup(
     packages=find_packages(),
     install_requires=['pillow>=4.3.0', 'multiprocess>=0.70.5', 'dill>=0.2.7'],
     python_requires='>=3.5',
-    description="Lightweight and fast Python library for simulating Chinese handwriting.",
+    description="A lightweight and fast Python library for simulating Chinese handwriting.",
     long_description=pylf.__doc__,
     license="bsd-3-clause",
     keywords="simulating Chinese handwriting",
