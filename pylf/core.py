91dfcb93ea3a96256749b0b2a754d1a651269244
Gsllchb <Gsllchb@gmail.com>
2017-11-24 16:57:49 +0800

    调整程序结构；使代码更紧凑。

--- pylf/core.py
+++ pylf/core.py
@@ -90,61 +90,33 @@ def handwrite(text, template: dict, anti_aliasing: bool=True, worker: int=0) -> 
     return _handwrite(text, template, anti_aliasing, worker)
 
 
-def _handwrite(text, template: dict, anti_aliasing: bool, worker: int):
-    settings = _get_settings(template, anti_aliasing)
-    outlines = _sketch(text, **settings)
+def _handwrite(text, template, anti_aliasing, worker):
+    outlines = _sketch(anti_aliasing, text, **template)
     # Because the FreeTypeFont object is NOT pickle-able, we can not accelerate _draw_chars() with multiprocess.
-    images = map(_draw_chars_factory(**settings), outlines)
+    images = map(_draw_chars_factory(anti_aliasing, **template), outlines)
     with multiprocess.Pool(worker) as p:
-        images = p.map(_perturb_factory(**settings), images)
-        if anti_aliasing:
-            images = p.map(_downsample, images)
-        images = p.map(_merge_factory(**settings), images)
+        images = p.map(_perturb_factory(anti_aliasing, **template), images)
+        images = p.map(_downsample_factory(anti_aliasing, **template), images)
+        images = p.map(_merge_factory(anti_aliasing, **template), images)
     return images
 
 
-def _get_settings(template: dict, anti_aliasing: bool):
-    """
-    Construct a dict to replace template for later processes.
-    :return: a dict containing settings.
-    """
-    if anti_aliasing:
-        template['box'] = tuple(2 * i for i in template['box'])
-        template['font_size'] *= 2
-        template['font_size_sigma'] *= 2
-        template['line_spacing'] *= 2
-        template['line_spacing_sigma'] *= 2
-        template['word_spacing'] *= 2
-        template['word_spacing_sigma'] *= 2
-        template['x_amplitude'] *= 2
-        template['y_amplitude'] *= 2
-        template['x_wavelength'] *= 2
-        template['y_wavelength'] *= 2
-        template['x_lambd'] /= 2
-        template['y_lambd'] /= 2
-    if anti_aliasing:
-        template['size'] = tuple(2 * i for i in template['background'].size)
-    else:
-        template['size'] = template['background'].size
-    return template
-
-
-def _sketch(text,
-            box: tuple,
-            font_size: int,
-            font_size_sigma: float,
-            line_spacing: int,
-            line_spacing_sigma: float,
-            word_spacing: int,
-            word_spacing_sigma: float,
-            is_end_char,
-            is_half_char,
-            **kwargs) -> list:
+def _sketch(anti_aliasing, text, box, font_size, font_size_sigma, line_spacing, line_spacing_sigma,
+            word_spacing, word_spacing_sigma, is_end_char, is_half_char, **kwargs):
     """
     Draw the outlines for later parallel computation.
     :return: outlines
         outlines is a list of lists containing tuples that respectively contain (x, y), char, actual_font_size in order.
     """
+    if anti_aliasing:
+        box = tuple(2 * i for i in box)
+        font_size *= 2
+        font_size_sigma *= 2
+        line_spacing *= 2
+        line_spacing_sigma *= 2
+        word_spacing *= 2
+        word_spacing_sigma *= 2
+
     length = len(text)
     outlines = []
     i = 0
@@ -171,10 +143,12 @@ def _sketch(text,
     return outlines
 
 
-def _draw_chars_factory(size, font, color: tuple, **kwargs):
+def _draw_chars_factory(anti_aliasing, background, font, color, **kwargs):
     """
     The factory of the function that draw chars on the foreground depending on the 'outline' provided by _sketch().
     """
+    size = tuple(2 * i for i in background.size) if anti_aliasing else background.size
+
     def _draw_chars(outline):
         image = PIL.Image.new('RGBA', size, color=(0, 0, 0, 0))
         draw = PIL.ImageDraw.Draw(image)
@@ -184,16 +158,23 @@ def _draw_chars_factory(size, font, color: tuple, **kwargs):
     return _draw_chars
 
 
-def _perturb_factory(x_amplitude, y_amplitude, x_wavelength, y_wavelength, x_lambd, y_lambd,  **kwargs):
+def _perturb_factory(anti_aliasing, x_amplitude, y_amplitude, x_wavelength, y_wavelength, x_lambd, y_lambd, **kwargs):
     """
     The factory of the function that 'perturb' the foreground image.
     """
+    if anti_aliasing:
+        x_amplitude *= 2
+        y_amplitude *= 2
+        x_wavelength *= 2
+        y_wavelength *= 2
+        x_lambd /= 2
+        y_lambd /= 2
+
     def _perturb(image):
         from math import sin, pi
         height = image.height
         width = image.width
         px = image.load()
-
         start = 0
         for x in range(width):
             if x >= start + x_wavelength:
@@ -205,7 +186,6 @@ def _perturb_factory(x_amplitude, y_amplitude, x_wavelength, y_wavelength, x_lam
                 px[x, y] = px[x, y + offset]
             for y in range(height - offset, height):
                 px[x, y] = (0, 0, 0, 0)
-
         start = 0
         for y in range(height):
             if y >= start + y_wavelength:
@@ -217,25 +197,30 @@ def _perturb_factory(x_amplitude, y_amplitude, x_wavelength, y_wavelength, x_lam
                 px[x, y] = px[x + offset, y]
             for x in range(width - offset, width):
                 px[x, y] = (0, 0, 0, 0)
-
         return image
     return _perturb
 
 
-def _downsample(image):
+def _downsample_factory(anti_aliasing, **kwargs):
     """
-    Downsampling for 4X SSAA.
+    The factory of the function downsampling for 4X SSAA.
     """
-    width, height = image.size[0] // 2, image.size[1] // 2
-    sampled_image = PIL.Image.new('RGBA', (width, height), color=(0, 0, 0, 0))
-    spx, px = sampled_image.load(), image.load()
-    for x in range(width):
-        for y in range(height):
-            spx[x, y] = tuple(sum(i)//4 for i in zip(px[2*x, 2*y], px[2*x+1, 2*y], px[2*x, 2*y+1], px[2*x+1, 2*y+1]))
-    return sampled_image
+    if anti_aliasing:
+        def _downsample(image):
+            width, height = image.size[0] // 2, image.size[1] // 2
+            sampled_image = PIL.Image.new('RGBA', (width, height), color=(0, 0, 0, 0))
+            spx, px = sampled_image.load(), image.load()
+            for x in range(width):
+                for y in range(height):
+                    temp = zip(px[2 * x, 2 * y], px[2 * x + 1, 2 * y], px[2 * x, 2 * y + 1], px[2 * x + 1, 2 * y + 1])
+                    spx[x, y] = tuple(sum(i) // 4 for i in temp)
+            return sampled_image
+        return _downsample
+    else:
+        return lambda image: image
 
 
-def _merge_factory(background, **kwargs):
+def _merge_factory(anti_aliasing, background, **kwargs):
     """
     The factory of the function that merge the image with the background.
     """
