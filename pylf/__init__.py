cc27832fce61b4297e39b4a6011ee158586d43eb
Gsllchb <Gsllchb@gmail.com>
2017-11-24 11:16:12 +0800

    update version to 0.2.2.

--- pylf/__init__.py
+++ pylf/__init__.py
@@ -3,4 +3,4 @@ PyLf is a lightweight Python library for simulating Chinese handwriting. It intr
 process of Chinese handwriting to simulate the uncertainty of glyphs written by human beings.
 """
 from .core import handwrite
-__version__ = '0.2.1'
+__version__ = '0.2.2'
