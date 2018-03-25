df176ce7d06d37e21f756ec27694a491b1fdba23
Gsllchb <Gsllchb@gmail.com>
2018-03-25 12:30:47 +0800

    minor change

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ import pylf
 classifiers = [
     'License :: OSI Approved :: BSD License',
     "Topic :: Multimedia :: Graphics",
+    "Programming Language :: Python",
     'Programming Language :: Python :: 3.5',
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: Implementation :: CPython"
