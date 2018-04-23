48bfc40ffff60b8d00cd42b3cd39dddbdc8e8859
Gsllchb <Gsllchb@gmail.com>
2018-04-23 11:05:58 +0800

    将long_description中的本地文件路径改为github上的路径

--- setup.py
+++ setup.py
@@ -2,8 +2,27 @@ from setuptools import setup, find_packages
 
 import pylf
 
-with open('README.md') as f:
-    long_description = f.read()
+
+VERSION = pylf.__version__
+PROJECT_URL_HEADER = "https://github.com/Gsllchb/PyLf/blob/v{}/".format(VERSION)
+
+
+def get_long_description() -> str:
+    file_paths = (
+        "docs/API-Reference.md",
+        "docs/CODE_OF_CONDUCT.md",
+        "docs/CONTRIBUTING.md",
+        "docs/ISSUE_TEMPLATE.md",
+        "docs/NEWS.md",
+        "LICENSE",
+        "README.md"
+    )
+    with open('README.md') as f:
+        long_description = f.read()
+    for file_path in file_paths:
+        long_description = long_description.replace(file_path, PROJECT_URL_HEADER + file_path)
+    return long_description
+
 
 setup(
     classifiers=[
@@ -16,7 +35,7 @@ setup(
         "Programming Language :: Python :: Implementation :: CPython"
     ],
     name='pylf',
-    version=pylf.__version__,
+    version=VERSION,
     author='Gsllchb',
     author_email="Gsllchb@gmail.com",
     packages=find_packages(),
@@ -24,7 +43,7 @@ setup(
     install_requires=['pillow >= 5.0.0, < 6', ],
     python_requires='>=3.5, <3.7',
     description="A lightweight Python library for simulating Chinese handwriting",
-    long_description=long_description,
+    long_description=get_long_description(),
     long_description_content_type='text/markdown',
     license="bsd-3-clause",
     keywords="simulating Chinese handwriting",
