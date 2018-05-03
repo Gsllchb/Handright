6461d57af6f48b7d78413b91a78e896882121824
Gsllchb <Gsllchb@gmail.com>
2018-05-03 10:30:30 +0800

    refactor test_handwrite2.py

--- tests/test_handwrite2.py
+++ tests/test_handwrite2.py
@@ -1,58 +1,34 @@
-import os
-
 import PIL.Image
 import PIL.ImageFont
+from util import *
 
-from pylf import handwrite2
+from pylf import *
 
+THESHOLD = 17.0
 
-def get_path(path: str) -> str:
-    return os.path.split(os.path.realpath(__file__))[0] + '/' + path
-
-
-def compare(im1, im2):
-    return im1.tobytes() == im2.tobytes()
-
-
-def get_default_text():
-    with open(get_path("data/texts/荷塘月色.txt")) as f:
-        return f.read()
+BACKGROUND_COLOR = 'rgb(255, 255, 255)'
+DEFAULT_WIDTH = 500
+DEFAULT_HEIGHT = 500
+DEFAULT_SIZE = (DEFAULT_WIDTH, DEFAULT_HEIGHT)
 
 
-def get_default_template2():
-    """ Get the template2 without randomness """
+def test_one_background():
+    background = PIL.Image.new(mode='RGB', size=DEFAULT_SIZE, color=BACKGROUND_COLOR)
+    box = (50, 100, DEFAULT_WIDTH - 50, DEFAULT_HEIGHT - 100)
+    font = get_default_font()
+    font_size = 30
+    font_size_sigma = 0
+
+    text = get_long_text()
+    template = dict(background=background, box=box, font=font, font_size=font_size, font_size_sigma=font_size_sigma)
     template2 = dict(
-        page_settings=[
-            dict(
-                background=PIL.Image.open(get_path("data/backgrounds/会议记录/even.png")),
-                box=(333, 1245, 2151, 3010),
-                font_size=95,
-                line_spacing=23,
-                font_size_sigma=0,
-                line_spacing_sigma=0,
-                word_spacing_sigma=0
-            ),
-            dict(
-                background=PIL.Image.open(get_path("data/backgrounds/会议记录/odd.png")),
-                box=(333, 535, 2151, 3010),
-                font_size=95,
-                line_spacing=23,
-                font_size_sigma=0,
-                line_spacing_sigma=0,
-                word_spacing_sigma=0
-            ),
-        ],
-        font=PIL.ImageFont.truetype(get_path(
-            "data/fonts/Bo Le Locust Tree Handwriting Pen Chinese Font-Simplified Chinese Fonts.ttf")),
-        alpha=(0, 0),
+        page_settings=[dict(background=background, box=box, font_size=font_size, font_size_sigma=font_size_sigma), ],
+        font=font,
     )
-    return template2
-
-
-def test_even_odd():
-    text = get_default_text()
-    template2 = get_default_template2()
-    ims = handwrite2(text, template2, anti_aliasing=False)
-    for i, im in enumerate(ims):
-        # im.save(get_path("data/images/test_even_odd{}.png".format(i)))
-        assert compare(im, PIL.Image.open(get_path("data/images/test_even_odd{}.png".format(i))))
+    for anti_aliasing in (True, False):
+        images1 = handwrite(text, template, anti_aliasing=anti_aliasing)
+        images2 = handwrite2(text, template2, anti_aliasing=anti_aliasing)
+        for im1, im2 in zip(images1, images2):
+            im1.show()
+            im2.show()
+            assert compare_histogram(im1, im2) < THESHOLD
