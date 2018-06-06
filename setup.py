dd98224eb9074f9a914a76faa4fccc5ab9b1351a
Gsllchb <Gsllchb@gmail.com>
2018-06-06 11:33:31 +0800

    refactor setup.py with the style guide

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
+import setuptools
 
 import pylf
 
-
 VERSION = pylf.__version__
+
+CLASSIFIERS = ("License :: OSI Approved :: BSD License",
+               "Topic :: Multimedia :: Graphics",
+               "Programming Language :: Python",
+               "Programming Language :: Python :: 3",
+               "Programming Language :: Python :: 3.4",
+               "Programming Language :: Python :: 3.5",
+               "Programming Language :: Python :: 3.6",
+               "Programming Language :: Python :: Implementation :: CPython")
+
+REQUIRES = ('setuptools>=38.6.0',
+            'pillow >= 5.0.0, < 6')
 
 
 def get_long_description() -> str:
@@ -12,32 +23,23 @@ def get_long_description() -> str:
         return f.read()
 
 
-classifiers=["License :: OSI Approved :: BSD License",
-             "Topic :: Multimedia :: Graphics",
-             "Programming Language :: Python",
-             "Programming Language :: Python :: 3",
-             "Programming Language :: Python :: 3.4",
-             "Programming Language :: Python :: 3.5",
-             "Programming Language :: Python :: 3.6",
-             "Programming Language :: Python :: Implementation :: CPython"]
+def main():
+    setuptools.setup(classifiers=CLASSIFIERS,
+                     name='pylf',
+                     version=VERSION,
+                     author='Gsllchb',
+                     author_email="Gsllchb@gmail.com",
+                     packages=setuptools.find_packages(),
+                     install_requires=REQUIRES,
+                     python_requires='>=3.4',
+                     description="A lightweight Python library for simulating Chinese handwriting",
+                     long_description=get_long_description(),
+                     long_description_content_type='text/markdown',
+                     license="bsd-3-clause",
+                     keywords="simulating Chinese handwriting",
+                     url="https://github.com/Gsllchb/PyLf",
+                     zip_safe=True)
 
 
-requires = ['setuptools>=38.6.0',
-            'pillow >= 5.0.0, < 6']
-
-
-setup(classifiers=classifiers,
-      name='pylf',
-      version=VERSION,
-      author='Gsllchb',
-      author_email="Gsllchb@gmail.com",
-      packages=find_packages(),
-      install_requires=requires,
-      python_requires='>=3.4',
-      description="A lightweight Python library for simulating Chinese handwriting",
-      long_description=get_long_description(),
-      long_description_content_type='text/markdown',
-      license="bsd-3-clause",
-      keywords="simulating Chinese handwriting",
-      url="https://github.com/Gsllchb/PyLf",
-      zip_safe=True)
+if __name__ == '__main__':
+    main()
