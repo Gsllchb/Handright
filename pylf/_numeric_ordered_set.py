65771c9faeef7021e5dd035f094efe5bcf48e1c3
Gsllchb <gsllchb@gmail.com>
2018-08-24 09:35:43 +0800

    create add_flag()

--- pylf/_numeric_ordered_set.py
+++ pylf/_numeric_ordered_set.py
@@ -6,19 +6,26 @@ import array
 class NumericOrderedSet(object):
     """This data structure only support storing numeric elements"""
 
-    def __init__(self, typecode: str):
-        """More info about typecode: https://docs.python.org/3/library/array.html#module-array"""
+    def __init__(self, typecode: str, flag):
+        """More info about typecode: https://docs.python.org/3/library/array.html#module-array
+        The value of flag must be within the typecode's range. Flag is invisible in set operations, but it will appear
+        in the ordered sequence."""
         self._typecode = typecode
+        self._flag = flag
         self._array = array.array(typecode)
         self._set = set()
 
     def add(self, item) -> bool:
         """This has no effect and would return False if the item is already present"""
+        assert item != self._flag
         if item in self._set:
             return False
         self._set.add(item)
         self._array.append(item)
         return True
+
+    def add_flag(self) -> None:
+        self._array.append(self._flag)
 
     def __contains__(self, item) -> bool:
         return item in self._set
@@ -31,9 +38,12 @@ class NumericOrderedSet(object):
         self._array = array.array(self._typecode)
 
     def __len__(self):
-        assert len(self._array) == len(self._set)
         return len(self._array)
 
     @property
     def typecode(self) -> str:
         return self._typecode
+
+    @property
+    def flag(self):
+        return self._flag
--- pylf/tests/test_numeric_ordered_set.py
+++ pylf/tests/test_numeric_ordered_set.py
@@ -1,35 +1,48 @@
 # coding: utf-8
 from pylf._numeric_ordered_set import NumericOrderedSet
-SIGNED_LONG = 'l'
+UNSIGNED_INT4 = 'L'
+MAX_UNSIGNED_INT4_VALUE = 0xFFFFFFFF
+
+
+def test_flag():
+    nos = NumericOrderedSet(UNSIGNED_INT4, MAX_UNSIGNED_INT4_VALUE)
+    assert MAX_UNSIGNED_INT4_VALUE == nos.flag
 
 
 def test_typecode():
-    typecode = SIGNED_LONG
-    nos = NumericOrderedSet(typecode)
-    assert nos.typecode == typecode
+    nos = NumericOrderedSet(UNSIGNED_INT4, MAX_UNSIGNED_INT4_VALUE)
+    assert nos.typecode == UNSIGNED_INT4
 
 
 def test_order():
-    seq = list(range(10))
-    nos = NumericOrderedSet(SIGNED_LONG)
-    for i in seq:
+    seq1 = list(range(10))
+    seq2 = list(range(10, 20, 2))
+    nos = NumericOrderedSet(UNSIGNED_INT4, MAX_UNSIGNED_INT4_VALUE)
+    for i in seq1:
         nos.add(i)
-    assert list(nos) == seq
+    nos.add_flag()
+    for i in seq2:
+        nos.add(i)
+    assert list(nos) == seq1 + [MAX_UNSIGNED_INT4_VALUE, ] + seq2
 
 
 def test_len():
-    nos = NumericOrderedSet(SIGNED_LONG)
+    nos = NumericOrderedSet(UNSIGNED_INT4, MAX_UNSIGNED_INT4_VALUE)
     length = 10
     for i in range(length):
         assert len(nos) == i
-        nos.add(i)
+        assert nos.add(i)
     assert len(nos) == length
+    nos.add_flag()
+    assert len(nos) == length + 1
+    nos.add_flag()
+    assert len(nos) == length + 2
     nos.clear()
     assert len(nos) == 0
 
 
 def test_add():
-    nos = NumericOrderedSet(SIGNED_LONG)
+    nos = NumericOrderedSet(UNSIGNED_INT4, MAX_UNSIGNED_INT4_VALUE)
     assert nos.add(1)
     assert nos.add(0)
     assert not nos.add(1)
@@ -37,4 +50,5 @@ def test_add():
     assert not nos.add(1)
     assert len(nos) == 2
     assert nos.add(2)
-    assert list(nos) == [1, 0, 2]
+    nos.add_flag()
+    assert list(nos) == [1, 0, 2, MAX_UNSIGNED_INT4_VALUE]
