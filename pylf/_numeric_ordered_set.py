65353ecb31f6374d331a50a5d632d425b347a820
Gsllchb <gsllchb@gmail.com>
2018-09-26 21:45:11 +0800

    change NumericOrderedSet to inherit from abc.Collection

--- pylf/_numeric_ordered_set.py
+++ pylf/_numeric_ordered_set.py
@@ -1,9 +1,12 @@
 # coding: utf-8
 """A customized data structure for perturbation algorithm"""
 import array
+from collections import abc
 
 
-class NumericOrderedSet(object):
+# This class is intended not to inherit from abc.MutableSet.
+# Unfortunately, we cannot directly use abc.Collection here. Because, abc.Collection is added after Python3.5.
+class NumericOrderedSet(abc.Sized, abc.Iterable, abc.Container):
     """This data structure only support storing numeric elements"""
     __slots__ = ("_typecode", "_flag", "_array", "_set")
 
