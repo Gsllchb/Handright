fe62bd0316b012d2bc8bad54d5ad06cfa59526d3
Gsllchb <gsllchb@gmail.com>
2018-09-27 22:23:17 +0800

    make parameter checking more appropriate

--- pylf/__init__.py
+++ pylf/__init__.py
@@ -237,12 +237,12 @@ def _check_parameters(text, template2, worker, seed) -> None:
 
 def _check_text(text) -> None:
     if not isinstance(text, abc.Iterable):
-        raise TypeError("'text' must be char iterable")
+        raise TypeError("'text' must be char Iterable")
 
 
 def _check_template2(template2) -> None:
     if not isinstance(template2, abc.Mapping):
-        raise TypeError("'template2' must be mapping")
+        raise TypeError("'template2' must be Mapping")
 
     length = len(template2["backgrounds"])
     if length <= 0:
@@ -254,7 +254,7 @@ def _check_template2(template2) -> None:
     # check backgrounds
     for b in template2["backgrounds"]:
         if not isinstance(b, image.Image):
-            raise TypeError("'background' must be Pillow's image")
+            raise TypeError("'background' must be Pillow's Image")
         if b.width > _MAX_IMAGE_SIDE_LENGTH:
             raise ValueError("The width of background cannot exceed {}".format(_MAX_IMAGE_SIDE_LENGTH))
         if b.height > _MAX_IMAGE_SIDE_LENGTH:
@@ -263,15 +263,15 @@ def _check_template2(template2) -> None:
     # check margins
     for m in template2["margins"]:
         for key in ("top", "bottom", "left", "right"):
-            if not isinstance(m[key], int):
-                raise TypeError("'margin[\"{}\"]' must be int".format(key))
+            if not isinstance(m[key], numbers.Integral):
+                raise TypeError("'margin[\"{}\"]' must be Integral".format(key))
             if m[key] < 0:
                 raise ValueError("'margin[\"{}\"]' must be at least 0".format(key))
 
     # check line_spacings
     for b, m, ls in zip(template2["backgrounds"], template2["margins"], template2["line_spacings"]):
-        if not isinstance(ls, int):
-            raise TypeError("'line_spacing' must be int")
+        if not isinstance(ls, numbers.Integral):
+            raise TypeError("'line_spacing' must be Integral")
         if ls <= 0:
             raise ValueError("'line_spacing' must be at least 1")
         if b.size[1] < m["top"] + ls + m["bottom"]:
@@ -281,8 +281,8 @@ def _check_template2(template2) -> None:
     # check font_sizes
     for b, m, ls, fs in zip(template2["backgrounds"], template2["margins"], template2["line_spacings"],
                             template2["font_sizes"]):
-        if not isinstance(fs, int):
-            raise TypeError("'font_size' must be int")
+        if not isinstance(fs, numbers.Integral):
+            raise TypeError("'font_size' must be Integral")
         if fs <= 0:
             raise ValueError("'font_size' must be at least 1")
         if fs > ls:
@@ -296,8 +296,8 @@ def _check_template2(template2) -> None:
         if len(template2["word_spacings"]) != length:
             raise ValueError("'word_spacings' and 'backgrounds' must have the same length")
         for ws, fs in zip(template2["word_spacings"], template2["font_sizes"]):
-            if not isinstance(ws, int):
-                raise TypeError("'word_spacing' must be int")
+            if not isinstance(ws, numbers.Integral):
+                raise TypeError("'word_spacing' must be Integral")
             if not ws > -fs // 2:
                 raise ValueError("'word_spacing' must be greater than (-font_size // 2)")
 
@@ -315,25 +315,25 @@ def _check_template2(template2) -> None:
             if len(template2[sigmas]) != length:
                 raise ValueError("'{}' and 'backgrounds' must have the same length".format(sigmas))
             for s in template2[sigmas]:
-                if not isinstance(s, (int, float)):
-                    raise TypeError("'{}' must be int or float".format(sigmas[:-1]))
+                if not isinstance(s, numbers.Real):
+                    raise TypeError("'{}' must be Real".format(sigmas[:-1]))
                 if s < 0:
                     raise ValueError("'{}' must be at least 0")
 
     # check *_fn
     for fn in ("is_half_char_fn", "is_end_char_fn"):
         if fn in template2:
-            if not callable(template2[fn]):
-                raise TypeError("'{}' must be callable".format(fn))
+            if not isinstance(template2[fn], abc.Callable):
+                raise TypeError("'{}' must be Callable".format(fn))
 
 
 def _check_worker(worker) -> None:
-    if not isinstance(worker, int):
-        raise TypeError("'worker' must be int")
+    if not isinstance(worker, numbers.Integral):
+        raise TypeError("'worker' must be Integral")
     if worker <= 0:
         raise ValueError("'worker' must be at least 1")
 
 
 def _check_seed(seed) -> None:
     if not isinstance(seed, abc.Hashable):
-        raise TypeError("'seed' must be hashable")
+        raise TypeError("'seed' must be Hashable")
