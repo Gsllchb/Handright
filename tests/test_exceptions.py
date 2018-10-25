ee0c379914841c29b6cff6b202238aa804074e8c
Gsllchb <gsllchb@gmail.com>
2018-10-25 21:32:49 +0800

    refactor tests/test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -5,97 +5,115 @@ import pytest
 from pylf import *
 from tests.util import *
 
-BACKGROUND_COLOR = "white"
-DEFAULT_WIDTH = 500
-DEFAULT_HEIGHT = 500
-DEFAULT_SIZE = (DEFAULT_WIDTH, DEFAULT_HEIGHT)
+WIDTH = 50
+HEIGHT = 50
+SIZE = (WIDTH, HEIGHT)
+FONT_SIZE = 3
+
 SEED = "PyLf"
-THRESHOLD = 0.01
+
+SUPPORTED_MODES = ("1", "L", "I", "F", "RGB", "RGBA")
+
+MAX_IMAGE_SIDE_LENGTH = 0xFFFF - 1
 
 
 def get_default_template() -> dict:
     template = {
-        "background": PIL.Image.new(
-            mode="RGB", size=DEFAULT_SIZE, color=BACKGROUND_COLOR
-        ),
-        "margin": {"left": 50, "top": 94, "right": 50, "bottom": 100},
-        "line_spacing": 36,
+        "background": PIL.Image.new("RGB", SIZE),
+        "margin": {"left": 5, "top": 9, "right": 5, "bottom": 10},
+        "line_spacing": 4,
         "font": get_default_font(),
-        "font_size": 30,
-        "font_size_sigma": 0,
+        "font_size": FONT_SIZE,
     }
     return template
 
 
 def get_default_template2() -> dict:
     template2 = {
-        "backgrounds": (
-            PIL.Image.new(mode="RGB", size=DEFAULT_SIZE, color="white"),
-            PIL.Image.new(mode="RGBA", size=DEFAULT_SIZE, color="rgb(0, 128, 255)"),
-        ),
+        "backgrounds": (PIL.Image.new("RGB", SIZE), PIL.Image.new("RGBA", SIZE)),
         "margins": (
-            {"left": 50, "top": 94, "right": 50, "bottom": 100},
-            {"left": 50, "top": 96, "right": 50, "bottom": 100},
+            {"left": 5, "top": 9, "right": 5, "bottom": 10},
+            {"left": 5, "top": 10, "right": 5, "bottom": 10},
         ),
-        "line_spacings": (36, 24),
-        "font_sizes": (30, 20),
-        "font_size_sigma": (0, 0),
+        "line_spacings": (4, 2),
+        "font_sizes": (FONT_SIZE, FONT_SIZE),
         "font": get_default_font(),
-        "color": "black",
     }
     return template2
 
 
-def test_text_error():
+def test_text():
     with pytest.raises(TypeError):
         handwrite(1, get_default_template())
     with pytest.raises(TypeError):
-        handwrite2(1, get_default_template2())
+        handwrite2(None, get_default_template2())
+    with pytest.raises(TypeError):
+        handwrite(list("123"), get_default_template())
+    with pytest.raises(TypeError):
+        handwrite2(tuple(""), get_default_template2())
 
 
-def test_worker_error():
+def test_worker():
     with pytest.raises(TypeError):
         handwrite("", get_default_template(), worker=3.3)
     with pytest.raises(TypeError):
-        handwrite2("", get_default_template2(), worker=3.3)
+        handwrite2("", get_default_template2(), worker=-3.3)
     with pytest.raises(ValueError):
         handwrite("", get_default_template(), worker=0)
     with pytest.raises(ValueError):
-        handwrite2("", get_default_template2(), worker=0)
+        handwrite2("", get_default_template2(), worker=-0)
 
 
-def test_seed_error():
+def test_seed():
     with pytest.raises(TypeError):
         handwrite("", get_default_template(), seed=[])
     with pytest.raises(TypeError):
-        handwrite2("", get_default_template2(), seed=[])
+        handwrite2("", get_default_template2(), seed={})
 
 
-def test_template_error():
-    template_error_helper("background", 1, TypeError)
-
-    template_error_helper(
-        "margin", {"left": 3.3, "right": 0, "top": 0, "bottom": 0}, TypeError
+def test_background():
+    helper("background", 1, TypeError)
+    helper("background", PIL.Image.new("1", (MAX_IMAGE_SIDE_LENGTH + 1, 1)), ValueError)
+    helper("background", PIL.Image.new("1", (1, MAX_IMAGE_SIDE_LENGTH + 1)), ValueError)
+    helper(
+        "background",
+        PIL.Image.new("1", (MAX_IMAGE_SIDE_LENGTH + 1, MAX_IMAGE_SIDE_LENGTH + 1)),
+        ValueError,
     )
-    template_error_helper(
-        "margin", {"left": 0, "right": -1, "top": 0, "bottom": 0}, ValueError
-    )
+    for mode in PIL.Image.MODES:
+        if mode in SUPPORTED_MODES:
+            continue
+        helper("background", PIL.Image.new(mode, SIZE), NotImplementedError)
 
-    template_error_helper("line_spacing", 1.2, TypeError)
-    template_error_helper("line_spacing", 0, ValueError)
-    template_error_helper("line_spacing", DEFAULT_HEIGHT * 2, LayoutError)
 
-    template_error_helper("font_size", 1.2, TypeError)
-    template_error_helper("font_size", 0, ValueError)
-    template_error_helper("font_size", DEFAULT_WIDTH * 2, LayoutError)
+def test_margin():
+    helper("margin", {"left": 3.3, "right": 0, "top": 0, "bottom": 0}, TypeError)
+    helper("margin", {"left": 0, "right": -1, "top": 0, "bottom": 0}, ValueError)
 
-    template_error_helper("word_spacing", 1.2, TypeError)
-    template_error_helper(
-        "word_spacing", (-get_default_template()["font_size"] // 2), LayoutError
-    )
 
-    template_error_helper("color", 0, TypeError)
+def test_line_spacing():
+    helper("line_spacing", 1.2, TypeError)
+    helper("line_spacing", 0, ValueError)
+    helper("line_spacing", HEIGHT * 2, LayoutError)
 
+
+def test_font_size():
+    helper("font_size", 1.2, TypeError)
+    helper("font_size", 0, ValueError)
+    helper("font_size", WIDTH * 2, LayoutError)
+
+
+def test_word_spacing():
+    helper("word_spacing", 1.2, TypeError)
+    helper("word_spacing", -FONT_SIZE // 2, LayoutError)
+
+
+def test_color():
+    helper("color", 0, TypeError)
+    helper("color", "2333333333", ValueError)
+
+
+def test_sigmas():
     for sigma in (
         "line_spacing_sigma",
         "font_size_sigma",
@@ -104,23 +122,17 @@ def test_template_error():
         "perturb_y_sigma",
         "perturb_theta_sigma",
     ):
-        template_error_helper(sigma, "1", TypeError)
-        template_error_helper(sigma, -1, ValueError)
+        helper(sigma, 1 + 2j, TypeError)
+        helper(sigma, -1, ValueError)
 
+
+def test_fns():
     for fn in ("is_half_char_fn", "is_end_char_fn"):
-        template_error_helper(fn, 0, TypeError)
+        helper(fn, 0, TypeError)
 
 
-def template_error_helper(key: str, value, error_type) -> None:
+def helper(key: str, value, exception_type) -> None:
     template = get_default_template()
     template[key] = value
-    with pytest.raises(error_type):
+    with pytest.raises(exception_type):
         handwrite("PyLf", template)
-
-
-def test_template2_error():
-    template2 = get_default_template2()
-    template2["word_spacings"] = (0,)
-    with pytest.raises(ValueError):
-        handwrite2("", template2)
-    # TODO: test all the parameters
