9367b60b3aacd9bdf22247d2b202b5ba74ba3b4f
Gsllchb <gsllchb@gmail.com>
2019-09-07 16:37:56 +0800

    improve code style

--- handright/_template.py
+++ handright/_template.py
@@ -82,23 +82,23 @@ class Template(object):
 
     def __eq__(self, other) -> bool:
         return (isinstance(other, Template)
-                and self.get_background() == other.get_background()
-                and self.get_line_spacing() == other.get_line_spacing()
-                and self.get_line_spacing_sigma() == other.get_line_spacing_sigma()
-                and self.get_font_size() == other.get_font_size()
-                and self.get_font_size_sigma() == other.get_font_size_sigma()
-                and self.get_font() == other.get_font()
-                and self.get_fill() == other.get_fill()
-                and self.get_left_margin() == other.get_left_margin()
-                and self.get_top_margin() == other.get_top_margin()
-                and self.get_right_margin() == other.get_right_margin()
-                and self.get_bottom_margin() == other.get_bottom_margin()
-                and self.get_word_spacing() == other.get_word_spacing()
-                and self.get_word_spacing_sigma() == other.get_word_spacing_sigma()
-                and self.get_end_chars() == other.get_end_chars()
-                and self.get_perturb_x_sigma() == other.get_perturb_x_sigma()
-                and self.get_perturb_y_sigma() == other.get_perturb_y_sigma()
-                and self.get_perturb_theta_sigma() == other.get_perturb_theta_sigma())
+                and self._background == other._background
+                and self._line_spacing == other._line_spacing
+                and self._line_spacing_sigma == other._line_spacing_sigma
+                and self._font_size == other._font_size
+                and self._font_size_sigma == other._font_size_sigma
+                and self._font == other._font
+                and self._fill == other._fill
+                and self._left_margin == other._left_margin
+                and self._top_margin == other._top_margin
+                and self._right_margin == other._right_margin
+                and self._bottom_margin == other._bottom_margin
+                and self._word_spacing == other._word_spacing
+                and self._word_spacing_sigma == other._word_spacing_sigma
+                and self._end_chars == other._end_chars
+                and self._perturb_x_sigma == other._perturb_x_sigma
+                and self._perturb_y_sigma == other._perturb_y_sigma
+                and self._perturb_theta_sigma == other._perturb_theta_sigma)
 
     def set_background(self, background: PIL.Image.Image) -> None:
         self._background = background
@@ -131,28 +131,40 @@ class Template(object):
     def set_top_margin(self, top_margin: int = _DEFAULT_TOP_MARGIN) -> None:
         self._top_margin = top_margin
 
-    def set_right_margin(self, right_margin: int = _DEFAULT_RIGHT_MARGIN) -> None:
+    def set_right_margin(
+            self, right_margin: int = _DEFAULT_RIGHT_MARGIN
+    ) -> None:
         self._right_margin = right_margin
 
-    def set_bottom_margin(self, bottom_margin: int = _DEFAULT_BOTTOM_MARGIN) -> None:
+    def set_bottom_margin(
+            self, bottom_margin: int = _DEFAULT_BOTTOM_MARGIN
+    ) -> None:
         self._bottom_margin = bottom_margin
 
-    def set_word_spacing(self, word_spacing: int = _DEFAULT_WORD_SPACING) -> None:
+    def set_word_spacing(
+            self, word_spacing: int = _DEFAULT_WORD_SPACING
+    ) -> None:
         self._word_spacing = word_spacing
 
-    def set_line_spacing_sigma(self, line_spacing_sigma: Optional[float] = None) -> None:
+    def set_line_spacing_sigma(
+            self, line_spacing_sigma: Optional[float] = None
+    ) -> None:
         if line_spacing_sigma is None:
             self._line_spacing_sigma = self._font_size / 32
         else:
             self._line_spacing_sigma = line_spacing_sigma
 
-    def set_font_size_sigma(self, font_size_sigma: Optional[float] = None) -> None:
+    def set_font_size_sigma(
+            self, font_size_sigma: Optional[float] = None
+    ) -> None:
         if font_size_sigma is None:
             self._font_size_sigma = self._font_size / 64
         else:
             self._font_size_sigma = font_size_sigma
 
-    def set_word_spacing_sigma(self, word_spacing_sigma: Optional[float] = None) -> None:
+    def set_word_spacing_sigma(
+            self, word_spacing_sigma: Optional[float] = None
+    ) -> None:
         if word_spacing_sigma is None:
             self._word_spacing_sigma = self._font_size / 32
         else:
@@ -161,19 +173,25 @@ class Template(object):
     def set_end_chars(self, end_chars: str = _DEFAULT_END_CHARS) -> None:
         self._end_chars = end_chars
 
-    def set_perturb_x_sigma(self, perturb_x_sigma: Optional[float] = None) -> None:
+    def set_perturb_x_sigma(
+            self, perturb_x_sigma: Optional[float] = None
+    ) -> None:
         if perturb_x_sigma is None:
             self._perturb_x_sigma = self._font_size / 32
         else:
             self._perturb_x_sigma = perturb_x_sigma
 
-    def set_perturb_y_sigma(self, perturb_y_sigma: Optional[float] = None) -> None:
+    def set_perturb_y_sigma(
+            self, perturb_y_sigma: Optional[float] = None
+    ) -> None:
         if perturb_y_sigma is None:
             self._perturb_y_sigma = self._font_size / 32
         else:
             self._perturb_y_sigma = perturb_y_sigma
 
-    def set_perturb_theta_sigma(self, perturb_theta_sigma: float = _DEFAULT_PERTURB_THETA_SIGMA) -> None:
+    def set_perturb_theta_sigma(
+            self, perturb_theta_sigma: float = _DEFAULT_PERTURB_THETA_SIGMA
+    ) -> None:
         self._perturb_theta_sigma = perturb_theta_sigma
 
     def get_background(self) -> PIL.Image.Image:
