0a141a04d0620e6bce303c415413c4297c5a4f30
Gsllchb <gsllchb@gmail.com>
2020-03-15 09:49:04 +0800

    optimize the API docs

--- handright/__init__.py
+++ handright/__init__.py
@@ -5,8 +5,16 @@ Created by Chenghui Li (Gsllchb).
 
 Licensed under BSD 3-Clause.
 """
-from handright._core import (
-    handwrite, Template, Error, LayoutError, BackgroundTooLargeError
-)
+from handright._core import handwrite
+from handright._exceptions import Error, LayoutError, BackgroundTooLargeError
+from handright._template import Template
 
 __version__ = "5.4.0"
+
+__all__ = (
+    "handwrite",
+    "Template",
+    "Error",
+    "LayoutError",
+    "BackgroundTooLargeError"
+)
