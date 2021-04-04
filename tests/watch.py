beaac48dbe7c39bcb784b09fdad6a0d5c2e40376
gsllchb <gsllchb@qq.com>
2021-04-04 21:20:23 +0800

    add show case for the grid layout

--- tests/watch.py
+++ tests/watch.py
@@ -1,6 +1,5 @@
 # coding: utf-8
 """ Doing the tests with your naked eyes """
-import pathlib
 
 import PIL.Image
 
@@ -11,8 +10,14 @@ SEED = "Handright"
 
 
 def main():
-    # TODO: Add case for grid layout
     print("Test by naked eyes:")
+    _watch_gird_layout()
+    _like_it()
+    _watch_flow_layout()
+    _like_it()
+
+
+def _watch_flow_layout():
     path = "backgrounds/even-odd-letter/"
     image1 = PIL.Image.open(abs_path(path + "村庄信笺纸.jpg"))
     image2 = PIL.Image.open(abs_path(path + "树信笺纸.jpg"))
@@ -38,14 +43,39 @@ def main():
         font=get_default_font(74),
     )
     templates = (template1, template2)
-    for file in pathlib.Path(abs_path("texts")).iterdir():
-        print(file)
-        with file.open(encoding="utf-8") as f:
-            text = f.read()
-        images = handwrite(text, templates, seed=SEED)
-        for im in images:
-            im.show()
-        assert input("Like it? [Y/N] ").upper() == "Y"
+    with open(abs_path("texts/从百草园到三味书屋.txt"), encoding="utf-8") as f:
+        text = f.read()
+    images = handwrite(text, templates, seed=SEED)
+    for im in images:
+        im.show()
+
+
+def _watch_gird_layout():
+    image = PIL.Image.open(abs_path("backgrounds/grid.jpeg"))
+    assert image.mode == "L"
+    template = Template(
+        background=image,
+        left_margin=0,
+        top_margin=0,
+        right_margin=0,
+        bottom_margin=0,
+        line_spacing=56,
+        word_spacing=4,
+        font=get_default_font(37),
+        font_size_sigma=0.3,
+        word_spacing_sigma=0.8,
+        line_spacing_sigma=1,
+        features={Feature.GRID_LAYOUT},
+    )
+    with open(abs_path("texts/荷塘月色.txt"), encoding="utf-8") as f:
+        text = f.read()
+    images = handwrite(text, template, seed=SEED)
+    for im in images:
+        im.show()
+
+
+def _like_it() -> None:
+    assert input("Like it? [Y/N] ").upper() == "Y"
 
 
 if __name__ == "__main__":
